a5e3ec8df017f07e4d99d3d259b2242299878dc2
Dhruv-0101 <d7511162@gmail.com>
2024-08-02 19:10:36 +0530

    minor-changes

--- contracts/10.7.conditionals.sol
+++ contracts/10.7.conditionals.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.2 <0.9.0;
 contract demo {
     function Conditionals(int256 a) public pure returns (int256) {
         if (a > 0) {
-            return 0;
+            return a;
         } else if (a < 0) {
             return -1;
         } else {
